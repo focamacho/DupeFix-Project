e400265d4fa7cc0e096c538c375ddae420c2c2fe
Renan Colombo <48870573+Focamacho@users.noreply.github.com>
2020-01-10 07:31:28 -0300

    1.1 Version

--- src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
+++ src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
@@ -22,7 +22,7 @@ public class DupeFixProject
 {
     public static final String MODID = "dupefixproject";
     public static final String NAME = "DupeFix Project";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static Logger logger;
     @EventHandler
