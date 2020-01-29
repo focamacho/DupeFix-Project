89b9a4cd48fc976c884f2c5b930664b7f6f403f0
Renan Colombo <48870573+Focamacho@users.noreply.github.com>
2020-01-29 12:11:18 -0300

    Better Fix for TConstruct Tool Forge Dupe

--- src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
+++ src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
@@ -24,7 +24,7 @@ public class DupeFixProject
 	
     public static final String MODID = "dupefixproject";
     public static final String NAME = "DupeFix Project";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3";
 
     public static Logger logger;
     @EventHandler
--- src/main/java/com/focamacho/dupefixproject/fixes/TConstructFixes.java
+++ src/main/java/com/focamacho/dupefixproject/fixes/TConstructFixes.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import slimeknights.tconstruct.tools.common.block.BlockToolForge;
 import slimeknights.tconstruct.tools.common.inventory.ContainerToolForge;
 
@@ -29,6 +31,29 @@ public class TConstructFixes {
 					}
 				}
 			}
+		}
+	}
+	
+	@SubscribeEvent
+	public void onTickPlayer(PlayerTickEvent event) {
+		if(event.player.openContainer != null && event.player.openContainer instanceof ContainerToolForge) {
+			if(!event.player.getEntityData().getBoolean("ToolForgeDupeFix")) {
+				BlockPos forgePos = ((ContainerToolForge) event.player.openContainer).getTile().getPos();
+				List<EntityPlayer> players = event.player.world.getEntitiesWithinAABB(EntityPlayer.class, new AxisAlignedBB(event.player.posX - 32, event.player.posY - 32, event.player.posZ - 32, event.player.posX + 32, event.player.posY + 32, event.player.posZ + 32));
+				for(EntityPlayer player : players) {
+					if(player != event.player && player.openContainer instanceof ContainerToolForge) {
+						ContainerToolForge container = (ContainerToolForge) player.openContainer;
+						if(container.getTile().getPos().equals(forgePos)) {
+							event.player.closeScreen();
+							event.player.sendMessage(new TextComponentString("Someone is already using this."));
+							break;
+						}
+					}
+				}
+				event.player.getEntityData().setBoolean("ToolForgeDupeFix", true);
+			}
+		} else {
+			event.player.getEntityData().setBoolean("ToolForgeDupeFix", false);
 		}
 	}
 }
