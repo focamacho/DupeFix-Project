d101d85b3c3db4e369214818471879f89f3953c1
Renan Colombo <renancolombomail@gmail.com>
2020-04-03 22:50:40 -0300

    Remove Astral Sorcery Fix

--- src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
+++ src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
@@ -14,7 +14,7 @@ public class DupeFixProject {
 	
     public static final String MODID = "dupefixproject";
     public static final String NAME = "DupeFix Project";
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "2.1";
 
     public static Logger logger;
     
@@ -63,11 +63,6 @@ public class DupeFixProject {
         if(Loader.isModLoaded("cyclicmagic")) {
         	MinecraftForge.EVENT_BUS.register(new CyclicFixes());
         	logger.info("Cyclic Fixes Loaded");
-        }
-        //Astral Sorcery
-        if(Loader.isModLoaded("astralsorcery")) {
-        	MinecraftForge.EVENT_BUS.register(new AstralSorceryFixes());
-        	logger.info("Astral Sorcery Fixes Loaded");
         }
         //Nether Chest
         if(Loader.isModLoaded("netherchest")) {
