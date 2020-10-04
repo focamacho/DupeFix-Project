abe08f567bbc3aa00e88868b272d69273f1f6558
Focamacho <renancolombomail@gmail.com>
2020-10-04 11:31:27 -0300

    Fix EnderIO not loaded fixes message. #48

--- src/main/java/com/focamacho/dupefixproject/util/LoadedFixes.java
+++ src/main/java/com/focamacho/dupefixproject/util/LoadedFixes.java
@@ -18,7 +18,10 @@ public class LoadedFixes {
 
     public static String getModFixesNotLoaded() {
         String modFixesNotLoaded = "";
-        if(Loader.isModLoaded("thaumcraft") && !LoadedFixes.thaumcraft) modFixesNotLoaded += "Thaumcraft, ";
+        if(Loader.isModLoaded("thaumcraft")) {
+            if(!LoadedFixes.thaumcraft) modFixesNotLoaded += "Thaumcraft, ";
+            if(Loader.isModLoaded("enderio") && !LoadedFixes.enderio) modFixesNotLoaded += "EnderIO, ";
+        }
         if(Loader.isModLoaded("bloodmagic") && !LoadedFixes.bloodMagic) modFixesNotLoaded += "Blood Magic, ";
         if(Loader.isModLoaded("extrautils2") && !LoadedFixes.extraUtilities) modFixesNotLoaded += "Extra Utilities 2, ";
         if(Loader.isModLoaded("actuallyadditions") && !LoadedFixes.actuallyAdditions) modFixesNotLoaded += "Actually Additions, ";
@@ -26,7 +29,6 @@ public class LoadedFixes {
         if(Loader.isModLoaded("tp") && !LoadedFixes.tinyProgressions) modFixesNotLoaded += "Tiny Progressions, ";
         if(Loader.isModLoaded("spiceoflife") && !LoadedFixes.spiceOfLife) modFixesNotLoaded += "Spice of Life, ";
         if(Loader.isModLoaded("netherchest") && !LoadedFixes.netherChest) modFixesNotLoaded += "NetherChest, ";
-        if(Loader.isModLoaded("enderio") && !LoadedFixes.enderio) modFixesNotLoaded += "EnderIO, ";
         if(Loader.isModLoaded("farlanders") && !LoadedFixes.theFarlanders) modFixesNotLoaded += "The Farlanders, ";
         if(Loader.isModLoaded("thaumicwonders") && !LoadedFixes.thaumicWonders) modFixesNotLoaded += "Thaumic Wonders, ";
         return modFixesNotLoaded.isEmpty() ? "" : modFixesNotLoaded.substring(0, modFixesNotLoaded.length() - 2);
