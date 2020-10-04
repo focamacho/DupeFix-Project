6f704d65b6baf94e0c41da5f7a4879a346780168
Focamacho <renancolombomail@gmail.com>
2020-10-04 17:09:45 -0300

    3.0.3

--- src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
+++ src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
@@ -15,7 +15,7 @@ public class DupeFixProject {
 	
     public static final String MODID = "dupefixproject";
     public static final String NAME = "DupeFix Project";
-    public static final String VERSION = "3.0.2";
+    public static final String VERSION = "3.0.3";
 
     public static Logger logger = LogManager.getLogger(NAME);
 
