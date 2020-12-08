9df92286ca5bb0b55c3757677662617bd4943e2a
Focamacho <renancolombomail@gmail.com>
2020-12-08 00:03:22 -0300

    Better Satchel Fix

--- src/main/java/com/focamacho/dupefixproject/mixin/thermalexpansion/ContainerSatchelFilterMixin.java
+++ src/main/java/com/focamacho/dupefixproject/mixin/thermalexpansion/ContainerSatchelFilterMixin.java
@@ -1,35 +1,26 @@
 package com.focamacho.dupefixproject.mixin.thermalexpansion;
 
 import cofh.core.gui.container.ContainerCore;
-import cofh.core.gui.slot.SlotLocked;
 import cofh.thermalexpansion.gui.container.storage.ContainerSatchelFilter;
-import cofh.thermalexpansion.item.ItemSatchel;
-import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.Slot;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ClickType;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(ContainerSatchelFilter.class)
 public abstract class ContainerSatchelFilterMixin extends ContainerCore {
 
     @Override
-    protected void bindPlayerInventory(InventoryPlayer inventoryPlayer) {
+    public ItemStack slotClick(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player) {
+        if(clickTypeIn == ClickType.SWAP) {
+            final ItemStack stack = player.inventory.getStackInSlot(dragType);
+            final ItemStack currentItem = player.inventory.getCurrentItem();
 
-        int xOffset = getPlayerInventoryHorizontalOffset();
-        int yOffset = getPlayerInventoryVerticalOffset();
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 9; j++) {
-                if(inventoryPlayer.getStackInSlot(j + i * 9 + 9).getItem() instanceof ItemSatchel) addSlotToContainer(new SlotLocked(inventoryPlayer, j + i * 9 + 9, xOffset + j * 18, yOffset + i * 18));
-                else addSlotToContainer(new Slot(inventoryPlayer, j + i * 9 + 9, xOffset + j * 18, yOffset + i * 18));
+            if (!currentItem.isEmpty() && stack == currentItem) {
+                return ItemStack.EMPTY;
             }
         }
-        for (int i = 0; i < 9; i++) {
-            if (i == inventoryPlayer.currentItem || inventoryPlayer.getStackInSlot(i).getItem() instanceof ItemSatchel) {
-                addSlotToContainer(new SlotLocked(inventoryPlayer, i, xOffset + i * 18, yOffset + 58));
-            } else {
-                addSlotToContainer(new Slot(inventoryPlayer, i, xOffset + i * 18, yOffset + 58));
-            }
-        }
+        return super.slotClick(slotId, dragType, clickTypeIn, player);
     }
 
 }
