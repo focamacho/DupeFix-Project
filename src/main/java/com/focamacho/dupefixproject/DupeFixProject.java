ba7cce628bfa509179de0c30cb42d63ca87b4efe
Focamacho <renancolombomail@gmail.com>
2021-08-13 08:30:10 -0300

    Downgrade the mixin version to avoid conflicts #97

--- src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
+++ src/main/java/com/focamacho/dupefixproject/DupeFixProject.java
@@ -16,7 +16,7 @@ public class DupeFixProject {
 
     public static final String MODID = "dupefixproject";
     public static final String NAME = "DupeFix Project";
-    public static final String VERSION = "3.1.5";
+    public static final String VERSION = "3.1.6";
 
     public static Logger logger = LogManager.getLogger(NAME);
 
